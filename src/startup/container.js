d7380de50a241a50a59843e52964b03769d19cbd
Douglas <dfjuarezreyes21@gmail.com>
2022-11-10 16:37:42 -0600

    models added

--- src/startup/container.js
+++ src/startup/container.js
@@ -12,6 +12,9 @@ const { HomeController } = require("../controllers");
 const { HomeRoutes } = require("../routes/index.routes");
 const Routes = require("../routes");
 
+// model
+const { User, Idea, Comment } = require("../models");
+
 const container = createContainer();
 
 container
@@ -28,6 +31,11 @@ container
   })
   .register({
     HomeRoutes: asFunction(HomeRoutes).singleton(),
+  })
+  .register({
+    User: asValue(User),
+    Idea: asValue(Idea),
+    Comment: asValue(Comment),
   });
 
 module.exports = container;
